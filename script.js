97ad70d7595176d8611de5877d19fdfc87a78ec4
umairzarar <you@example.com>
2024-05-22 17:10:13 +0500

    first message

---
+++ script.js
@@ -0,0 +1,7 @@
+function generateTable(number, length) {
+    for (var i = 1; i <= 20; i++) {
+        console.log(number + " * " + i + " = " + (number * i));
+        
+    }
+}
+generateTable(5,20);
